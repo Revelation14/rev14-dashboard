c86635541b49c518bf3ebcc225f4298c8bf1098b
knowbee <knowbeeinc@gmail.com>
2024-10-18 23:45:43 +0200

    fix: production baseUrl

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,13 +5,14 @@ import axios from 'axios';
 import { getFromLocalStorage } from './helper';
 
 const http = axios.create({
-  // baseURL: 'https://api.graceministries.online/api/v1',
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://api.graceministries.online/api/v1',
+  // baseURL: 'http://localhost:3000/api/v1',
   headers: {
     Authorization: '',
     'Content-Type': 'application/json',
   },
 });
+
 http.defaults.withCredentials = false;
 http.interceptors.request.use((config) => {
   const token = JSON.parse(getFromLocalStorage('token'));
